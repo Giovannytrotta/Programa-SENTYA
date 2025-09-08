faae99603474887923b7b37a4c84e333691714ff
Giovanny trotta <giovannytrotta13@gmail.com>
2025-09-08 19:05:41 +0200

    Terminada la authenticacion de usuario se ha completado todo el diseño de modelo de base de datos se ha verificado y corregido bugs. Se ha agregado los endpoint a usar para creacion de nuevo usuario, logeo mas authenticacion en 2 pasos, y endpoint privado para todo el panel de administrador de usuario

--- apps/backend/app/models/__init__.py
+++ apps/backend/app/models/__init__.py
@@ -1,3 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 
-from .user import User
+from .user import SystemUser
+from .workshops import Workshop
+from .workshop_users import WorkshopUser
+from .thematic_areas import ThematicArea
+from .sessions import Session
+from .css import Css
+from .attendance import Attendance
+from .audit_logs import AuditLog
